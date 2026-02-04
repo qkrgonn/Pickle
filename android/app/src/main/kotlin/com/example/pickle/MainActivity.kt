26ae55fd0cb348e6d24dd6d4dfdae559517ba145
박고운 <gounpark7475@gmail.com>
2026-02-04 17:07:44 +0900

    안드로이드통로설정 및 엔진수리/데이터 통로 설계

--- android/app/src/main/kotlin/com/example/pickle/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pickle/MainActivity.kt
@@ -1,6 +1,40 @@
 package com.example.pickle
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.pickle/share"
+    private var sharedText: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 1. 공유받은 텍스트(URL) 가로채기
+        val intent = intent
+        val action = intent.action
+        val type = intent.type
+
+        if (Intent.ACTION_SEND == action && type != null) {
+            if ("text/plain" == type) {
+                sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+            }
+        }
+    }
+
+    override fun configureFlutterEngine(flutterEngine: io.flutter.embedding.engine.FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        // 2. 자바(네이티브) 데이터를 Dart(플러터)로 보내는 통로 개설
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getSharedText") {
+                result.success(sharedText)
+                sharedText = null // 데이터를 보낸 후 초기화
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
